6b4fce49b7e1946623bbb6c0ed3df52077c607cc
Shashank Bharadwaj <shashankbharadwajj@gmail.com>
2019-03-13 23:25:44 +0530

    update1

--- pages/jobdetails.js
+++ pages/jobdetails.js
@@ -2,18 +2,6 @@ import React from "react";
 import JobDetails from "../components/JobDetails";
 import User from "../components/common/User";
 
-const JobDetailsPage = () => (
-  <User>
-    {({ data, loading, error }) => {
-      if (loading) {
-        return <div>Loading</div>;
-      }
-      if (data) {
-        return <JobDetails />;
-      }
-      return <div>Not Authorised</div>;
-    }}
-  </User>
-);
+const JobDetailsPage = () => (<Home />);
 
 export default JobDetailsPage;
